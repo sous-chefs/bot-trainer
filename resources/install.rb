5db0f6b9d87b3ac473a2c98b358c58e0c2106191
Jason Field <jason@avon-lea.co.uk>
2021-08-17 10:53:41 +0100

    unified_mode

--- resources/install.rb
+++ resources/install.rb
@@ -16,7 +16,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+unified_mode true
 property :version, String, default: 'latest', description: 'Version of firefox to download.'
 property :lang, String, default: 'en-US', description: 'Language of firefox to install. Windows and Mac OS X only'
 property :package_name, String, default: lazy { firefox_package_name }, description: 'Package name to install'
